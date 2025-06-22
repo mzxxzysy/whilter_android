2ba665a9699b03e41a17de209f4d22cea26a4adb
mzxxzysy <jsyjj21@gmail.com>
2025-06-22 14:42:55 +0900

    스플래시 애니메이션 추가

--- app/src/main/java/com/example/myproject/SplashActivity.kt
+++ app/src/main/java/com/example/myproject/SplashActivity.kt
@@ -2,23 +2,47 @@ package com.example.myproject
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.Looper
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import androidx.lifecycle.lifecycleScope
-import kotlinx.coroutines.launch
 import java.util.concurrent.Executor
 import java.util.concurrent.Executors
 import java.util.concurrent.ScheduledExecutorService
 import java.util.concurrent.TimeUnit
 
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView
+
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        val imageView = findViewById<ImageView>(R.id.imageView)
+        val scaleAnimation = AnimationUtils.loadAnimation(this, R.anim.scale_anim)
+        val translateAnimation = AnimationUtils.loadAnimation(this, R.anim.translate_anim)
+
+        scaleAnimation.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationStart(animation: Animation?) {}
+
+            override fun onAnimationEnd(animation: Animation?) {
+                imageView.startAnimation(scaleAnimation)
+            }
+
+            override fun onAnimationRepeat(animation: Animation?) {}
+        })
+
+        translateAnimation.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationStart(animation: Animation?) {}
+
+            override fun onAnimationEnd(animation: Animation?) {
+            }
+
+            override fun onAnimationRepeat(animation: Animation?) {}
+        })
+
+        imageView.startAnimation(scaleAnimation)
+        imageView.startAnimation(translateAnimation)
 
         val backgroundExecutor: ScheduledExecutorService =
             Executors.newSingleThreadScheduledExecutor()
@@ -30,6 +54,6 @@ class SplashActivity : AppCompatActivity() {
                     startActivity(intent)
                     finish()
                 }
-            }, 2, TimeUnit.SECONDS)
+            }, 3, TimeUnit.SECONDS)
         }
     }
